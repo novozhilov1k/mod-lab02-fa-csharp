0864639725352d9fa5d09bc9968f9c31281b3b4e
novozhilov1k <novozhilovkirill793@gmail.com>
2026-03-30 13:39:12 +0300

    popitka1

--- fa.Tests/UnitTest1.cs
+++ fa.Tests/UnitTest1.cs
@@ -7,92 +7,212 @@ namespace NET
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod1_FA1()
         {
             String s = "0111";
             FA1 fa = new FA1();
             bool? result = fa.Run(s);
             Assert.IsTrue(result == true);
         }
+        
         [TestMethod]
-        public void TestMethod2()
+        public void TestMethod2_FA1()
         {
             String s = "01011";
             FA1 fa = new FA1();
             bool? result = fa.Run(s);
             Assert.IsTrue(result == false);
-        }   
+        }
+        
         [TestMethod]
-        public void TestMethod3()
+        public void TestMethod3_FA1()
         {
             String s = "110101011";
             FA1 fa = new FA1();
             bool? result = fa.Run(s);
             Assert.IsTrue(result == false);
-        }  
+        }
+        
         [TestMethod]
-        public void TestMethod4()
+        public void TestMethod4_FA1()
         {
             String s = "1110111";
             FA1 fa = new FA1();
             bool? result = fa.Run(s);
             Assert.IsTrue(result == true);
-        }  
+        }
+        
         [TestMethod]
-        public void TestMethod5()
+        public void TestMethod5_FA1()
         {
             String s = "10";
             FA1 fa = new FA1();
             bool? result = fa.Run(s);
             Assert.IsTrue(result == true);
-        } 
+        }
+        
+        // Дополнительные тесты для FA1
         [TestMethod]
-        public void TestMethod6()
+        public void TestMethod6_FA1()
+        {
+            String s = "011";
+            FA1 fa = new FA1();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == true);
+        }
+        
+        [TestMethod]
+        public void TestMethod7_FA1()
+        {
+            String s = "0";
+            FA1 fa = new FA1();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == false);
+        }
+        
+        [TestMethod]
+        public void TestMethod8_FA1()
+        {
+            String s = "111";
+            FA1 fa = new FA1();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == false);
+        }
+        
+        [TestMethod]
+        public void TestMethod9_FA2()
         {
             String s = "0101";
             FA2 fa = new FA2();
             bool? result = fa.Run(s);
             Assert.IsTrue(result == false);
-        }  
+        }
+        
         [TestMethod]
-        public void TestMethod7()
+        public void TestMethod10_FA2()
         {
             String s = "00110011";
             FA2 fa = new FA2();
             bool? result = fa.Run(s);
             Assert.IsTrue(result == false);
-        }  
+        }
+        
         [TestMethod]
-        public void TestMethod8()
+        public void TestMethod11_FA2()
         {
             String s = "0001";
             FA2 fa = new FA2();
             bool? result = fa.Run(s);
             Assert.IsTrue(result == true);
-        }  
+        }
+        
         [TestMethod]
-        public void TestMethod9()
+        public void TestMethod12_FA2()
         {
             String s = "111000";
             FA2 fa = new FA2();
             bool? result = fa.Run(s);
             Assert.IsTrue(result == true);
-        } 
+        }
+        
+        // Дополнительные тесты для FA2
         [TestMethod]
-        public void TestMethod10()
+        public void TestMethod13_FA2()
+        {
+            String s = "01";
+            FA2 fa = new FA2();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == true);
+        }
+        
+        [TestMethod]
+        public void TestMethod14_FA2()
+        {
+            String s = "0011";
+            FA2 fa = new FA2();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == false);
+        }
+        
+        [TestMethod]
+        public void TestMethod15_FA2()
+        {
+            String s = "000111";
+            FA2 fa = new FA2();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == true);
+        }
+
+        [TestMethod]
+        public void TestMethod16_FA3()
         {
             String s = "00110011";
             FA3 fa = new FA3();
             bool? result = fa.Run(s);
             Assert.IsTrue(result == true);
         }
+        
         [TestMethod]
-        public void TestMethod11()
+        public void TestMethod17_FA3()
         {
             String s = "0101";
             FA3 fa = new FA3();
             bool? result = fa.Run(s);
             Assert.IsTrue(result == false);
-        }     
+        }
+
+        [TestMethod]
+        public void TestMethod18_FA3()
+        {
+            String s = "11";
+            FA3 fa = new FA3();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == true);
+        }
+        
+        [TestMethod]
+        public void TestMethod19_FA3()
+        {
+            String s = "1101";
+            FA3 fa = new FA3();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == true);
+        }
+        
+        [TestMethod]
+        public void TestMethod20_FA3()
+        {
+            String s = "011";
+            FA3 fa = new FA3();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == true);
+        }
+        
+        [TestMethod]
+        public void TestMethod21_FA3()
+        {
+            String s = "101010";
+            FA3 fa = new FA3();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == false);
+        }
+        
+        [TestMethod]
+        public void TestMethod22_FA3()
+        {
+            String s = "111";
+            FA3 fa = new FA3();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == true);
+        }
+        
+        [TestMethod]
+        public void TestMethod23_FA3()
+        {
+            String s = "";
+            FA3 fa = new FA3();
+            bool? result = fa.Run(s);
+            Assert.IsTrue(result == false);
+        }
     }
 }
--- fa/program.cs
+++ fa/program.cs
@@ -16,25 +16,215 @@ namespace fans
 
   public class FA1
   {
+    //s0 начало, s1 есть 1 но нет 0, s2 есть 0 нет 1, s3 есть оба, s4 2 или больше нуля
+    public static State s0 = new State()
+    {
+      Name = "s0",
+      IsAcceptState = false,
+      Transitions = new Dictionary<char, State>()
+    };
+    public State s2 = new State()
+    {
+      Name = "s2",
+      IsAcceptState = false,
+      Transitions = new Dictionary<char, State>()
+    };
+    public State s1 = new State()
+    {
+      Name = "s1",
+      IsAcceptState = false,
+      Transitions = new Dictionary<char, State>()
+    };
+    public State s3 = new State()
+    {
+      Name = "s3",
+      IsAcceptState = true,
+      Transitions = new Dictionary<char, State>()
+    };
+    public State s4 = new State()
+    {
+      Name = "s4",
+      IsAcceptState = false,
+      Transitions = new Dictionary<char, State>()
+    };
+    State InitialState = s0;
+    public FA1()
+    {
+      s0.Transitions['0'] = s2;
+      s0.Transitions['1'] = s1;
+
+      s1.Transitions['0'] = s3;
+      s1.Transitions['1'] = s1;
+
+      s2.Transitions['1'] = s3;
+      s2.Transitions['0'] = s4;
+
+      s3.Transitions['0'] = s4;
+      s3.Transitions['1'] = s3;
+
+      s4.Transitions['0'] = s4;
+      s4.Transitions['1'] = s4;
+    }
     public bool? Run(IEnumerable<char> s)
     {
-      return false;
+      State cur = InitialState;
+      foreach(shar ss in s)
+      {
+        if(ss != "1" && ss != "0")
+        {
+          return null;
+        }
+
+        if (!current.Transitions.ContainsKey(c)) return null;
+
+        cur = cur.Transitions[ss];
+      }
+
+      return cur.IsAcceptState;
     }
   }
 
   public class FA2
   {
+    public static State s0 = new State()
+    {
+      Name = "s0",
+      IsAcceptState = false,
+      Transitions = new Dictionary<char, State>()
+    };
+     public State s1 = new State()
+    {
+      Name = "s1",
+      IsAcceptState = false,
+      Transitions = new Dictionary<char, State>()
+    };
+    public State s2 = new State()
+    {
+      Name = "s2",
+      IsAcceptState = false,
+      Transitions = new Dictionary<char, State>()
+    };
+    public State s3 = new State()
+    {
+      Name = "s3",
+      IsAcceptState = true,
+      Transitions = new Dictionary<char, State>()
+    };
+    public State s4 = new State()
+    {
+      Name = "s4",
+      IsAcceptState = false,
+      Transitions = new Dictionary<char, State>()
+    };
+    public State s5 = new State()
+    {
+      Name = "s5",
+      IsAcceptState = false,
+      Transitions = new Dictionary<char, State>()
+    };
+    public State s6 = new State()
+    {
+      Name = "s6",
+      IsAcceptState = false,
+      Transitions = new Dictionary<char, State>()
+    };
+    State InitialState = s0;
+
+    //s0 - начало, s1 первая/нечетное кол 1, s2 первая/нечетное кол 0, s3 все ок оба подходят, 
+    //s4 нечетное 1 четное 0, s5 четное 1 нечетное 0, s6 оба четные 
+    public FA2()
+    {
+      s0.Transitions['0'] = s2;
+      s0.Transitions['1'] = s1;
+
+      s1.Transitions['0'] = s3;
+      s1.Transitions['1'] = s0;
+
+      s2.Transitions['1'] = s3;
+      s2.Transitions['0'] = s0;
+
+      s3.Transitions['0'] = s4;
+      s3.Transitions['1'] = s5;
+
+      s4.Transitions['0'] = s3;
+      s4.Transitions['1'] = s6;
+
+      s5.Transitions['0'] = s6;
+      s5.Transitions['1'] = s3;
+
+      s6.Transitions['0'] = s5;
+      s6.Transitions['1'] = s4;
+    }
     public bool? Run(IEnumerable<char> s)
     {
-      return false;
+      State cur = InitialState;
+      foreach(shar ss in s)
+      {
+        if(ss != "1" && ss != "0")
+        {
+          return null;
+        }
+
+        if (!current.Transitions.ContainsKey(c)) return null;
+
+        cur = cur.Transitions[ss];
+      }
+
+      return cur.IsAcceptState;
     }
   }
   
   public class FA3
   {
+    public static State s0 = new State()
+    {
+      Name = "s0",
+      IsAcceptState = false,
+      Transitions = new Dictionary<char, State>()
+    };
+     public State s1 = new State()
+    {
+      Name = "s1",
+      IsAcceptState = false,
+      Transitions = new Dictionary<char, State>()
+    };
+    public State s2 = new State()
+    {
+      Name = "s2",
+      IsAcceptState = true,
+      Transitions = new Dictionary<char, State>()
+    };
+    State InitialState = s0;
+    //s0 старт/последний был 0, s1 первая 1, s2 11 встретилось 
+    public FA3()
+    {
+      s0.Transitions['0'] = s0;
+      s0.Transitions['1'] = s1;
+
+      s1.Transitions['0'] = s0;
+      s1.Transitions['1'] = s2;
+      
+      s2.Transitions['0'] = s2;
+      s2.Transitions['1'] = s2;
+      
+    }
+
     public bool? Run(IEnumerable<char> s)
     {
-      return false;
+      State cur = InitialState;
+      foreach(shar ss in s)
+      {
+        if(ss != "1" && ss != "0")
+        {
+          return null;
+        }
+
+        if (!current.Transitions.ContainsKey(c)) return null;
+
+        cur = cur.Transitions[ss];
+      }
+
+      return cur.IsAcceptState;
     }
   }
 
